3744c6005b98bb9fda5dabeef84bf548211984a9
Michiel Borkent <michielborkent@gmail.com>
2023-02-25 16:26:52 +0100

    Change to play sound without midi file

--- src/borkdude/clj_native_sound_demo.clj
+++ src/borkdude/clj_native_sound_demo.clj
@@ -1,14 +1,16 @@
 (ns borkdude.clj-native-sound-demo
-  (:require [clojure.java.io :as io])
-  (:gen-class))
-
-(import '[javax.sound.midi MidiSystem])
+  (:gen-class)
+  (:import
+   (javax.sound.midi MidiSystem)))
 
 (set! *warn-on-reflection* true)
 
-(defn -main [& args]
-  (let [sequencer (MidiSystem/getSequencer)]
-    (.open sequencer)
-    (.setSequence sequencer (io/input-stream (io/file (first args))))
-    (.start sequencer)))
-
+(defn -main [& _args]
+  (let [midi-synth (MidiSystem/getSynthesizer)
+        _ (.open midi-synth)
+        instruments (.getInstruments (.getDefaultSoundbank midi-synth))
+        midi-channels (.getChannels midi-synth)]
+    (.loadInstrument midi-synth ^javax.sound.midi.Instrument (first instruments))
+    (.noteOn ^javax.sound.midi.MidiChannel (first midi-channels) 60 100)
+    (Thread/sleep 1000)
+    (.noteOff ^javax.sound.midi.MidiChannel (first midi-channels) 60)))
